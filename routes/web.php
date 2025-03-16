a5b9bda8a3147f29a9455cf355a03cec38c9f049
wiktor <kowalinski.wiktor04@gmail.com>
2025-03-16 21:25:16 +0100

    add websocket client package

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use WebSocket\Client;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/test', function (Request $request) {
+    try {
+        $client = new Client("wss://stream.binance.com:9443/ws/btcusdt@trade");
+
+        while (true) {
+            $data = json_decode($client->receive(), true);
+            dump($data);
+            sleep(2);
+        }
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage();
+    }
 });
